52ac4b6d24786b620e70a98db5b63d4fbef0c01d
stalbot8 <65781297+stalbot8@users.noreply.github.com>
2023-06-13 00:26:12 -0400

    Add files via upload

---
+++ Project_puzzle_box_v1.ino
@@ -0,0 +1,75 @@
+#include <FastLED.h>
+#include <LEDMatrix.h>
+#include <LEDText.h>
+#include <FontMatrise.h>
+
+#define LED_PIN        13
+#define COLOR_ORDER    GRB
+#define CHIPSET        WS2812B
+
+#define MATRIX_WIDTH   24
+#define MATRIX_HEIGHT  8
+#define MATRIX_TYPE    VERTICAL_MATRIX
+
+cLEDMatrix<MATRIX_WIDTH, MATRIX_HEIGHT, MATRIX_TYPE> leds;
+
+cLEDText ScrollingMsg;
+const unsigned char TxtDemo[] = { EFFECT_FRAME_RATE "\x02" EFFECT_SCROLL_LEFT "    WE'RE PREGNANT! LOVE, KATIE & SPENCER "};
+
+//for smoothing
+const int numReadings = 10;
+
+int readings[numReadings];  // the readings from the analog input
+int readIndex = 0;          // the index of the current reading
+int total = 0;              // the running total
+int average = 0;            // the average
+
+int inputPin = A5;
+const int threshold = 300;
+
+void setup() {
+  FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds[0], leds.Size());
+  FastLED.setBrightness(64);
+  FastLED.clear(true);
+  ScrollingMsg.SetFont(MatriseFontData);
+  ScrollingMsg.Init(&leds, leds.Width(), ScrollingMsg.FontHeight() + 1, 0, 0);
+
+  // initialize all the readings to 0:
+  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+    readings[thisReading] = 0;
+  }
+  Serial.begin(9600);
+}
+
+void loop() {
+  // subtract the last reading:
+  total = total - readings[readIndex];
+  // read from the sensor:
+  readings[readIndex] = analogRead(inputPin);
+  // add the reading to the total:
+  total = total + readings[readIndex];
+  // advance to the next position in the array:
+  readIndex = readIndex + 1;
+
+  // if we're at the end of the array...
+  if (readIndex >= numReadings) {
+    // ...wrap around to the beginning:
+    readIndex = 0;
+  }
+
+  // calculate the average:
+  average = total / numReadings;
+  // send it to the computer as ASCII digits
+  Serial.println(average);
+  delay(1);  // delay in between reads for stability
+
+  if(average>threshold) {
+    if (ScrollingMsg.UpdateText() == -1) {
+      ScrollingMsg.SetTextColrOptions(COLR_HSV | COLR_SINGLE, random(0,255), 0xaa, 0xaa);
+      ScrollingMsg.SetText((unsigned char *)TxtDemo, sizeof(TxtDemo) - 1);
+    } else {
+      FastLED.show();
+    }
+    delay(13);
+  }
+}
